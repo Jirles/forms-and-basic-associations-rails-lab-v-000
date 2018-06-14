24d014ede7a95a44261bdd6360046e1b286fe68b
Emily Jirles <emily.jirles@gmail.com>
2018-06-14 20:07:35 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,35 +2,34 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-  
+
   #genre_name
-  def genre_name 
-    self.genre.name 
-  end 
-  
+  def genre_name
+    self.genre.name
+  end
+
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
-  end 
-  
-  #artist_name 
-  def artist_name 
-    self.artist.name 
-  end 
-  
+  end
+
+  #artist_name
+  def artist_name
+    Artist.find(self.artist_id).name 
+  end
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
-  end 
-  
+  end
+
   #note_contents
   def note_contents
     self.notes.collect{ |note| note.content }
-  end 
-  
-  def note_contents=(content)
-    content.each do |note_content| 
-      self.notes << Note.find_or_create_by(content: note_content) unless note_content.empty?
-    end 
-  end 
-  
-end
+  end
 
+  def note_contents=(content)
+    content.each do |note_content|
+      self.notes << Note.find_or_create_by(content: note_content) unless note_content.empty?
+    end
+  end
+
+end
